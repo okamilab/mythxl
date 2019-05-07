b92aa95c7fa90557c30b5b05433ae252644e12f4
Sergii Bomko <aquila.ua@gmail.com>
2019-05-07 21:38:23 +0200

    adds state manager

--- func/MythXL.Func/MythX/AnalysisExecutionPolicy.cs
+++ func/MythXL.Func/MythX/AnalysisExecutionPolicy.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
-using MythXL.Func.Entities;
+using MythXL.Func.Utils;
 using System;
 using System.Threading.Tasks;
 
@@ -10,14 +8,13 @@ namespace MythXL.Func.MythX
 {
     public class AnalysisExecutionPolicy
     {
-        private CloudTable _table;
-        private string _stateKey;
-
         public string Account { get; private set; }
 
-        private readonly string _url;
         private readonly IConfigurationRoot _config;
         private readonly ILogger _log;
+        private readonly string _url;
+        private readonly string _selectedAccountKey;
+        private readonly StateManager _stateManager;
 
         public AnalysisExecutionPolicy(IConfigurationRoot config, ILogger log)
         {
@@ -25,17 +22,14 @@ namespace MythXL.Func.MythX
             _log = log;
 
             _url = _config.GetValue<string>("MythX:BaseUrl");
-
-            _table = CreateTable(
-                config.GetValue<string>("Storage:Connection"),
-                config.GetValue<string>("Storage:AppStateTable"));
-            _stateKey = config.GetValue<string>("MythX:AccountManager:SelectedAccount");
+            _selectedAccountKey = config.GetValue<string>("MythX:AccountManager:SelectedAccount");
+            _stateManager = new StateManager(config);
         }
 
         public async Task<string> AnalyzeAsync(string code)
         {
             var accounts = new AccountManager(_config);
-            var state = await GetAccount();
+            var state = await _stateManager.GetValueAsync(_selectedAccountKey);
             var account = accounts.Get(accounts.IndexOf(state.Value));
             var attempts = 0;
 
@@ -52,42 +46,12 @@ namespace MythXL.Func.MythX
                     _log.LogDebug($"Account limit exceeded {ex.Address}");
                     account = accounts.Next(account.Address);
                     _log.LogDebug($"Switched to {account.Address}");
-                    await SetAccount(account.Address);
+                    await _stateManager.SetValueAsync(_selectedAccountKey, account.Address);
                     attempts++;
                 }
             }
 
             throw new Exception("Limit exceeded for all accounts");
-        }
-
-        // TODO: move to state manager
-        private async Task<StateEntity> GetAccount()
-        {
-            var filter = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, _stateKey);
-            var query = new TableQuery<StateEntity> { TakeCount = 1, FilterString = filter };
-            var queryResult = await _table.ExecuteQuerySegmentedAsync(query, null);
-            return queryResult.Results[0];
-        }
-
-        private async Task SetAccount(string address)
-        {
-            var entry = new StateEntity()
-            {
-                PartitionKey = _stateKey,
-                RowKey = "",
-                Value = address
-            };
-            TableOperation insertOperation = TableOperation.InsertOrReplace(entry);
-            await _table.ExecuteAsync(insertOperation);
-        }
-
-        private CloudTable CreateTable(string connection, string tableName)
-        {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connection);
-            var tableClient = storageAccount.CreateCloudTableClient();
-            var table = tableClient.GetTableReference(tableName);
-            table.CreateIfNotExistsAsync();
-            return table;
         }
     }
 }
---
+++ func/MythXL.Func/Utils/StateManager.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using MythXL.Func.Entities;
+using System.Threading.Tasks;
+
+namespace MythXL.Func.Utils
+{
+    public class StateManager
+    {
+        private CloudTable _table;
+
+        public StateManager(IConfigurationRoot config)
+        {
+            _table = CreateTable(
+                config.GetValue<string>("Storage:Connection"),
+                config.GetValue<string>("Storage:AppStateTable"));
+        }
+
+        public async Task<StateEntity> GetValueAsync(string key)
+        {
+            var filter = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, key);
+            var query = new TableQuery<StateEntity> { TakeCount = 1, FilterString = filter };
+            var queryResult = await _table.ExecuteQuerySegmentedAsync(query, null);
+            return queryResult.Results[0];
+        }
+
+        public async Task SetValueAsync(string key, string value)
+        {
+            var entry = new StateEntity()
+            {
+                PartitionKey = key,
+                RowKey = "",
+                Value = value
+            };
+            TableOperation insertOperation = TableOperation.InsertOrReplace(entry);
+            await _table.ExecuteAsync(insertOperation);
+        }
+
+        private CloudTable CreateTable(string connection, string tableName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connection);
+            var client = storageAccount.CreateCloudTableClient();
+            var table = client.GetTableReference(tableName);
+            table.CreateIfNotExistsAsync();
+            return table;
+        }
+    }
+}
